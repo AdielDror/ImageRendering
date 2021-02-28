61c59fe54944429543bec6295f7789642f8884a5
idovi <idovi@DESKTOP-STT5DLT>
2021-02-28 21:12:38 +0200

    First commit

--- src/primitives/Coordinate.java
+++ src/primitives/Coordinate.java
@@ -1,5 +1,27 @@
 package primitives;
 
 public class Coordinate {
+	double coord;
+	
+	
 
+	public Coordinate(double coord) {
+		setCoord(coord);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+	
+		return super.equals(obj);
+	}
+
+	public double getCoord() {
+		return coord;
+	}
+
+	public void setCoord(double coord) {
+		this.coord = coord;
+	}
+	
+	
 }
